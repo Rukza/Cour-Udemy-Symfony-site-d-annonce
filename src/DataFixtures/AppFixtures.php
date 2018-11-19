23d4f817fb4451efe153706416666397ec58fcc7
Kza <Rukza@orange.fr>
2018-11-19 16:10:17 +0100

    avant fixtures author

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -13,9 +14,31 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
+        //nous gerons les utilisateurs
+        $users = [];
 
-        
-        for($i = 1; $i <= 30; $i++){
+        for($i = 1; $i <= 10; $i++){
+
+            $user = new User();
+
+            $user->setFirstName($faker->firstname)
+                 ->setLastName($faker->lastName)
+                 ->setEmail($faker->email)
+                 ->setIntroduction($faker->sentence())
+                 ->setDescription('<p>' . join('</p><p>',$faker->paragraphs(3)) . '</p>')
+                 ->setHash('password');
+
+
+                $manager->persist($user);
+                $users[] = $user;
+
+        }
+
+
+
+
+        //nous gerons les annonces
+            for($i = 1; $i <= 30; $i++){
             $ad = new Ad();
 
             $title = $faker->sentence();
@@ -23,14 +46,15 @@ class AppFixtures extends Fixture
             $introduction = $faker->paragraph(2);
             $content = '<p>' . join('</p><p>',$faker->paragraphs(5)) . '</p>';
             
-
+                $user = $users[mt_rand(0, count($users) -1)];
 
                 $ad ->setTitle($title)
                     ->setCoverImage($coverImage)
                     ->setIntroduction( $introduction)
                     ->setContent($content)
                     ->setPrice(mt_rand(40, 200))
-                    ->setRooms(mt_rand(1 ,5));
+                    ->setRooms(mt_rand(1 ,5))
+                    ->setAuthor($user);
 
                 for($j = 1; $j <= mt_rand(2, 5); $j++) {
                     $image = new Image();
